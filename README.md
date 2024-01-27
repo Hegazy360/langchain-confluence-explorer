fb06aecf5f6732e209c95937f1c65fdbaa59723a
Mohamed Hegazy <m.hegazy94@hotmail.com>
2024-01-27 21:55:01 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# LangChain PDF Explorer
+
+This project is a Streamlit application that allows users to upload a PDF, ask a question about the content of the PDF, and get an answer based on the content of the PDF. The application uses OpenAI to generate embeddings for the text in the PDF and to answer the user's question.
+
+<img width="1000" alt="Screenshot 2024-01-26 at 14 20 13" src="https://github.com/Hegazy360/langchain-pdf-explorer/assets/13141632/d526c674-8340-483d-87be-b404e1aaa3a4">
+
+
+
+## Dependencies
+
+The project uses the following Python libraries:
+
+- streamlit
+- PyPDF2
+- langchain
+- OpenAI LLM
+
+## How to Run
+
+1. Use python venv `python -m venv .venv` & `source .venv/bin/activate`
+2. Install the dependencies `pip install -r requirements.txt`
+3. Set the necessary environment variables using a `.env` file.
+4. Run the Streamlit application using the command `streamlit run app.py`.
+
+## Code Structure
+
+The `app.py` file contains the main function of the application. It first loads the environment variables, then sets up the Streamlit application. It allows the user to upload a PDF file, then reads the file and extracts the text from it. The text is split into chunks, and embeddings are generated for each chunk. The user can then ask a question about the PDF, and the application will search for similar documents in the knowledge base and use a question answering chain to generate a response.
+
+## Future Work
+
+Future improvements could include better handling of PDF files with complex layouts, support for multiple languages, and improvements to the question answering algorithm.
---
+++ app.py
@@ -0,0 +1,67 @@
+from dotenv import load_dotenv
+import os
+import streamlit as st
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain_openai import OpenAI
+from langchain_community.callbacks import get_openai_callback
+from langchain_community.document_loaders import ConfluenceLoader
+
+def main():
+    # Load environment variables
+    load_dotenv()
+    confluence_api_key = os.getenv('CONFLUENCE_API_KEY')
+    confluence_username = os.getenv('CONFLUENCE_USERNAME')
+    confluence_url = os.getenv('CONFLUENCE_URL')
+
+    st.set_page_config(page_title="LangChain Confluence Explorer")
+    st.header("LangChain Confluence Explorer")
+
+    if 'chunks' not in st.session_state:
+        st.session_state['chunks'] = []
+    if 'knowledge_base' not in st.session_state:
+        st.session_state['knowledge_base'] = None
+
+    if st.button("Fetch Confluence Data", type="primary"):
+        loader = ConfluenceLoader(url=confluence_url, username=confluence_username, api_key=confluence_api_key)
+        documents = loader.load(
+            space_key="KB", include_attachments=True, limit=5, max_pages=10
+        )
+        for document in documents:
+            text_splitter = CharacterTextSplitter(
+                separator="\n",
+                chunk_size=1000,
+                chunk_overlap=200,
+                length_function=len
+            )
+            st.session_state['chunks'].extend(text_splitter.split_text(document.page_content))
+
+        if st.session_state['chunks']:
+            embeddings = OpenAIEmbeddings()
+            st.session_state['knowledge_base'] = FAISS.from_texts(st.session_state['chunks'], embeddings)
+
+    if st.session_state['knowledge_base'] is not None:
+        user_question = st.text_input("Ask a question about your Confluence space:")
+        print(st.session_state['knowledge_base'])
+
+        if user_question:
+            print(user_question)
+            docs = st.session_state['knowledge_base'].similarity_search(user_question)
+
+            llm = OpenAI()
+            chain = load_qa_chain(llm, chain_type="stuff")
+            input_data = {"input_documents": docs, "question": user_question}
+
+            with get_openai_callback() as cb:
+                response = chain.invoke(input=input_data)
+                print(cb)
+                cb.total_cost = cb.total_cost.__round__(7)
+                st.write(cb)
+
+            st.write(response['output_text'])
+
+# Run the main function
+if __name__ == '__main__':
+    main()
